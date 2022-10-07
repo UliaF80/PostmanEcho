65f7d8ec8881f2a7312300e20913b2b70d69d605
Юлия Фёдорова <uliafedorova297@gmail.com>
2022-10-07 22:38:42 +0300

    add *

---
+++ src/test/java/ReguestTest.java
@@ -0,0 +1,30 @@
+
+import org.junit.jupiter.api.Test;
+
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ReguestTest {
+
+
+    @Test
+    void PostTest() {
+
+         given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("") );
+    }
+
+
+
+
+}
